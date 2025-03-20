72724a89696c45e0bcb1b890361530198d60aeeb
HiddeXA <hiddealberts@gmail.com>
2025-03-20 12:01:57 +0100

    psuh

--- lessen/week-3/les-1/opdracht-hoger-lager/studentversie/frontend/scripts/hogerlager-feature.js
+++ lessen/week-3/les-1/opdracht-hoger-lager/studentversie/frontend/scripts/hogerlager-feature.js
@@ -7,18 +7,24 @@ class HogerLagerFeature{
         console.log('HogerLagerFeature init');
 
         //#student-start
+        this.eventListeners();
         //#student-end
 
     }
 
     eventListeners(){
-        document.getElementById('btnSubmit')
-            .addEventListener('click', this.submitGuess);
+        document.getElementById('form')
+            .addEventListener('submit', this.submitGuess);
     }
 
     async submitGuess(event){
+        event.preventDefault();
 
     //#student-start
+    fetch ('http://localhost:3000/getal?gok=' + document.getElementById('guess').value).then(response => response.json()).then(data => {
+        console.log(data);
+        document.getElementById('answer').innerText = data.result;
+    });
     //#student-end
 
     }
